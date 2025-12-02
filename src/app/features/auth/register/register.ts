e5c269e4eafa895c97c70eafcf551354a47b3f9b
BlackDenker <dadiva122016@gmail.com>
2025-12-02 18:41:40 -0500

    COreccion de errores en register.ts

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -4,7 +4,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -25,6 +28,9 @@ import { RouterLink } from '@angular/router';
 })
 export class Register {
   registerForm: FormGroup;
+  loading = false;
+  errorMessage = '';
+  successMessage = '';
 
   constructor(
     private fb: FormBuilder,
