fd7aad236c881732db11ec3f63899571e2cef0ea
BlackDenker <dadiva122016@gmail.com>
2025-12-02 18:33:54 -0500

    Se añadio peso ideal y enfermedades al registro de usuario

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -14,7 +15,9 @@ import { RouterLink } from '@angular/router';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+
     MatButtonModule,
+    MatSelectModule,
     RouterLink
   ],
   templateUrl: './register.html',
@@ -22,6 +25,7 @@ import { RouterLink } from '@angular/router';
 })
 export class Register {
   registerForm: FormGroup;
+  healthConditions: string[] = ['Alérgico a lácteos', 'Sin gluten', 'Alergia a nueces', 'Vegano', 'Vegetariano'];
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
@@ -30,7 +34,10 @@ export class Register {
       email: ['', [Validators.required, Validators.email]],
       age: ['', [Validators.required, Validators.min(1), Validators.max(120)]],
       height: ['', [Validators.required, Validators.min(1)]], // cm
+
       weight: ['', [Validators.required, Validators.min(1)]], // kg
+      idealWeight: ['', [Validators.required, Validators.min(1)]], // kg
+      healthTags: [[], [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required]
     }, { validators: this.passwordMatchValidator });
